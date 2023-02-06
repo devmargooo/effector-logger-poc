403ad72fbda51c1b3a85a755a13006735ff85f6e
Margo <luckinamargo@gmail.com>
2023-02-06 16:45:15 +0300

    effector logger POC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {createEvent, createStore} from 'effector-logger'
+import {useStore} from 'effector-react'
+import {createDomain} from 'effector';
+import {attachLogger} from 'effector-logger/attach'
 
-function App() {
+const plus = createEvent("plus");
+
+export const root = createDomain('app')
+const $counter = createStore({
+    value: 1,
+    square: 1,
+}, { name: "counter" }).on(plus, state => {
+    const value = state.value + 1;
+    return {
+        value: value,
+        square: value * value,
+    }
+})
+
+attachLogger(root, { reduxDevtools: 'enabled', console: 'enabled', inspector: 'enabled' });
+
+const App = () => {
+  const counter = useStore($counter)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <button onClick={() => plus()}>Plus</button>
+        <div>counter: {counter.value}, square: {counter.square}</div>
+      </div>
+  )
 }
 
 export default App;
